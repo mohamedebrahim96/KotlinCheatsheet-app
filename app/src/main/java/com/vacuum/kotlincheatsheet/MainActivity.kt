371d6751ac68917689e7802f8f7599bd1e21c007
mohamed ebrahim <ebrahimm131@gmail.com>
2018-08-04 00:21:29 +0200

    add navigation drawer

--- app/src/main/java/com/vacuum/kotlincheatsheet/MainActivity.kt
+++ app/src/main/java/com/vacuum/kotlincheatsheet/MainActivity.kt
@@ -1,43 +1,73 @@
 package com.vacuum.kotlincheatsheet
 
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.FragmentActivity
-import android.util.Log
-import android.widget.Toast
 import android.content.pm.PackageManager
-import android.content.Intent
-import android.content.ActivityNotFoundException
-import android.graphics.Color
-import android.net.Uri
+import android.content.res.Configuration
+import android.support.design.widget.NavigationView
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
-import android.view.View
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.widget.Toast
 import com.github.barteksc.pdfviewer.PDFView
 import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle
-import com.vacuum.kotlincheatsheet.R.id.pdfView
-import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     var pdfView:PDFView? = null
-    private val TAG = MainActivity::class.java!!.getSimpleName()
-    private val REQUEST_CODE = 42
     val PERMISSION_CODE = 42042
-    val SAMPLE_FILE = "sample.pdf"
     val READ_EXTERNAL_STORAGE = "android.permission.READ_EXTERNAL_STORAGE"
-    var uri: Uri? = null
-    var pageNumber: Int? = 0
-    var pdfFileName: String? = null
-
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         pdfView  = findViewById(R.id.pdfView)
+
+        navigathion_drawer()
         pickFile()
     }
+
+    private fun navigathion_drawer() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+        drawer = findViewById(R.id.drawer_layout)
+        toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close)
+        drawer.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_item_one -> Toast.makeText(this, "Clicked item one", Toast.LENGTH_SHORT).show()
+            R.id.nav_item_two -> Toast.makeText(this, "Clicked item two", Toast.LENGTH_SHORT).show()
+            R.id.nav_item_three -> Toast.makeText(this, "Clicked item three", Toast.LENGTH_SHORT).show()
+            R.id.nav_item_four -> Toast.makeText(this, "Clicked item four", Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
     fun pickFile() {
         val permissionCheck = ContextCompat.checkSelfPermission(this,
                 READ_EXTERNAL_STORAGE)
@@ -51,7 +81,9 @@ class MainActivity : AppCompatActivity() {
         displayFromAsset()
     }
     private fun displayFromAsset() {
-        pdfView!!.fromAsset("sample.pdf")
+        //kotlincheatsheet.pdf
+        //kotlin-full.pdf
+        pdfView!!.fromAsset("kotlin-full.pdf")
                 .defaultPage(0)
                 .enableAnnotationRendering(true)
                 .scrollHandle(DefaultScrollHandle(this))
