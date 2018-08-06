ffec3e25443e34024f9a8b5fb372272c422e8614
mohamed ebrahim <ebrahimm131@gmail.com>
2018-08-06 21:04:48 +0200

    fix bugs

--- app/src/main/java/com/vacuum/kotlincheatsheet/MainActivity.kt
+++ app/src/main/java/com/vacuum/kotlincheatsheet/MainActivity.kt
@@ -72,7 +72,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
         })
     }
-
     private fun prepareListData() {
         listDataHeader = ArrayList()
         val cheatsheet = ArrayList<Item>()
