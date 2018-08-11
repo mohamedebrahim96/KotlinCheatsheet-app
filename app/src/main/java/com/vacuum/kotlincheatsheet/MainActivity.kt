4b2664e96da49668f027ad3a42a8a6338240bd84
mohamed ebrahim <ebrahimm131@gmail.com>
2018-08-12 00:34:14 +0200

    Google Play Release V2.6: - KotlinCheatSheet - kotlin full documentation - Kotlin Essentials - Programming Kotlin - Fix Bugs - About Us

--- app/src/main/java/com/vacuum/kotlincheatsheet/MainActivity.kt
+++ app/src/main/java/com/vacuum/kotlincheatsheet/MainActivity.kt
@@ -39,6 +39,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         mContext = this
         pdfView  = findViewById(R.id.pdfView)
 
